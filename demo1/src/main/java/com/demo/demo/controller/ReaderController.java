ce8b2cb58de78536ccc88d98fa66d85708de426c
luoyanling1999 <1773888797@qq.com>
2020-09-22 14:21:18 +0800

    aa

--- demo1/src/main/java/com/demo/demo/controller/ReaderController.java
+++ demo1/src/main/java/com/demo/demo/controller/ReaderController.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping("/Test")
+@RequestMapping("/Tesat")
 public class ReaderController {
     @Autowired
     private readerService readerSer;
